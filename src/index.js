1022a933919dc0b95dd085e5f5a3fd4f12e92a85
Deborah Mayamba <deborah.mayamba@heig-vd.ch>
2023-03-17 11:21:30 +0100

    Modif JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 
  import data from '../data/DataKillers.csv'
+
+ //Création de mon canva SVG
+let svg = select("body")
+.append("svg")
+.attr("width", 1200)
+.attr("height", 350)
+.style('background-color', 'purple')
