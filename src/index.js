7d8e4761dca4c6de88918cb15868e4157920a4f3
romanoe <noemi.romano@protonmail.com>
2023-03-24 09:42:32 +0100

    right tree scaffolding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
+import data from '../data/DataKillers.csv';
 
- import data from '../data/DataKillers.csv'
-
- //Création de mon canva SVG
-let svg = select("body")
-.append("svg")
-.attr("width", 1200)
-.attr("height", 350)
-.style('background-color', 'purple')
+console.log(data)
